39bb9c7779a126b48a324161f4ed90e726ee7d93
Kunal Mandalia <kunal.v.mandalia@gmail.com>
2020-07-05 10:48:14 +0100

    fix: conditional route rendering

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,36 +1,46 @@
-   /*     File Name   : dashboard.js
+/*     File Name   : dashboard.js
           Date        : 31st May 2020
           Description : This is the Dashboard.js file which is a component file for the main screen
   */
-import React, { PureComponent } from 'react';
-import Question from './Question';
-import { TabContent, TabPane, Nav, NavItem, NavLink, Row, Col } from 'reactstrap';
-import classnames from 'classnames';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
+import React, { PureComponent } from 'react'
+import Question from './Question'
+import {
+  TabContent,
+  TabPane,
+  Nav,
+  NavItem,
+  NavLink,
+  Row,
+  Col,
+} from 'reactstrap'
+import classnames from 'classnames'
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
 
 class DashBoard extends PureComponent {
   state = {
-    activeTab: '1'
-  };
+    activeTab: '1',
+  }
 
   toggle(tab) {
     if (this.state.activeTab !== tab) {
       this.setState({
-        activeTab: tab
-      });
+        activeTab: tab,
+      })
     }
   }
 
   render() {
-    const { unansweredQuestions, answeredQuestions } = this.props;
+    const { unansweredQuestions, answeredQuestions } = this.props
     return (
       <div>
         <Nav tabs>
           <NavItem>
             <NavLink
               className={classnames({ active: this.state.activeTab === '1' })}
-              onClick={() => { this.toggle('1'); }}
+              onClick={() => {
+                this.toggle('1')
+              }}
             >
               Unanswered
             </NavLink>
@@ -38,7 +48,9 @@ class DashBoard extends PureComponent {
           <NavItem>
             <NavLink
               className={classnames({ active: this.state.activeTab === '2' })}
-              onClick={() => { this.toggle('2'); }}
+              onClick={() => {
+                this.toggle('2')
+              }}
             >
               Answered
             </NavLink>
@@ -48,41 +60,43 @@ class DashBoard extends PureComponent {
         <TabContent activeTab={this.state.activeTab}>
           <TabPane tabId="1">
             <Row>
-              {unansweredQuestions.map(qid =>
+              {unansweredQuestions.map((qid) => (
                 <Col key={qid} sm="6" md="4">
-                  <Question id={qid}/>
+                  <Question id={qid} />
                 </Col>
-              )}
+              ))}
             </Row>
           </TabPane>
           <TabPane tabId="2">
             <Row>
-              {answeredQuestions.map(qid =>
+              {answeredQuestions.map((qid) => (
                 <Col key={qid} sm="6" md="4">
-                  <Question id={qid}/>
+                  <Question id={qid} />
                 </Col>
-              )}
+              ))}
             </Row>
           </TabPane>
         </TabContent>
       </div>
-    );
+    )
   }
 }
 
 DashBoard.propTypes = {
-  answeredPolls : PropTypes.array,
-  unansweredPolls : PropTypes.array
-};
+  answeredPolls: PropTypes.array,
+  unansweredPolls: PropTypes.array,
+}
 
-function mapStateToProps ({ questions, users, authedUser }) {
-  const user = users[authedUser];
-  const answeredQuestions = Object.keys(user.answers)
-    .sort((a,b) => questions[b].timestamp - questions[a].timestamp);
+function mapStateToProps({ questions, users, authedUser }) {
+  const user = users[authedUser]
+  const answeredQuestions = Object.keys(user.answers).sort(
+    (a, b) => questions[b].timestamp - questions[a].timestamp
+  )
   return {
-    unansweredQuestions : Object.keys(questions).filter(qid => !answeredQuestions.includes(qid))
-      .sort((a,b) => questions[b].timestamp - questions[a].timestamp),
-    answeredQuestions
+    unansweredQuestions: Object.keys(questions)
+      .filter((qid) => !answeredQuestions.includes(qid))
+      .sort((a, b) => questions[b].timestamp - questions[a].timestamp),
+    answeredQuestions,
   }
 }
 
